b20b5c0bf993f4a1071e80a3527af8c617e3b153
ristinovelisa23-ops <risti.novelisa23@smk.belajar.id>
2025-12-23 12:53:07 +0800

    Update scriptusp.js

--- scriptusp.js
+++ scriptusp.js
@@ -7,31 +7,39 @@ function hitung() {
     return;
   }
 
-  // Mean
   let total = data.reduce((a, b) => a + b, 0);
   let mean = (total / data.length).toFixed(2);
 
-  // Median
-  data.sort((a, b) => a - b);
-  let mid = Math.floor(data.length / 2);
-  let median;
-  if (data.length % 2 === 0) {
-    median = (data[mid - 1] + data[mid]) / 2;
-  } else {
-    median = data[mid];
-  }
+  let sorted = [...data].sort((a, b) => a - b);
+  let mid = Math.floor(sorted.length / 2);
+  let median = sorted.length % 2 === 0
+    ? (sorted[mid - 1] + sorted[mid]) / 2
+    : sorted[mid];
 
-  // Modus
-  let frekuensi = {};
-  data.forEach(n => frekuensi[n] = (frekuensi[n] || 0) + 1);
-  let max = Math.max(...Object.values(frekuensi));
-  let modus = Object.keys(frekuensi).filter(n => frekuensi[n] === max).join(", ");
+  let freq = {};
+  sorted.forEach(n => freq[n] = (freq[n] || 0) + 1);
+  let max = Math.max(...Object.values(freq));
+  let modus = Object.keys(freq).filter(n => freq[n] == max).join(", ");
 
-  document.getElementById("mean").innerText = "Mean: " + mean + " jam";
-  document.getElementById("median").innerText = "Median: " + median + " jam";
-  document.getElementById("modus").innerText = "Modus: " + modus + " jam";
+  document.getElementById("mean").innerText = mean + " jam";
+  document.getElementById("median").innerText = median + " jam";
+  document.getElementById("modus").innerText = modus + " jam";
 
-  // PLAY AUDIO
+  document.getElementById("penjelasan").innerHTML = `
+    <b>Penjelasan:</b><br><br>
+
+    <b>Mean (Rata-rata)</b><br>
+    = (${sorted.join(" + ")}) ÷ ${sorted.length}<br>
+    = ${total} ÷ ${sorted.length} = <b>${mean} jam</b><br><br>
+
+    <b>Median (Nilai Tengah)</b><br>
+    Data diurutkan: ${sorted.join(", ")}<br>
+    Karena jumlah data ${sorted.length}, nilai tengahnya adalah <b>${median} jam</b><br><br>
+
+    <b>Modus</b><br>
+    Nilai yang paling sering muncul adalah <b>${modus} jam</b>
+  `;
+  
   const audio = document.getElementById("bgAudio");
   audio.play();
 }
